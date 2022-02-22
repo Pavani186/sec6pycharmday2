7dacdce7efe3f30dbf647d124dc3fc28c9f4ed3c
PAVANI <pavani1862002@gmail.com>
2022-02-22 16:34:16 +0530

    addition of two numbers

---
+++ main.py
@@ -0,0 +1,14 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def add(a,b):
+    print(a+b)
+
+
+
+
+# Press the green button in the gutter to run the script..
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(add(2,3))
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
